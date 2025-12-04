4533d14294ffee42bde9ee240da6c73de5205774
Tiago <tiagogocor@gmail.com>
2025-12-03 19:17:44 -0600

    correción

--- src/app/screens/registro-screen/registro-screen.component.ts
+++ src/app/screens/registro-screen/registro-screen.component.ts
@@ -1,79 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../../services/usuarios.service';
-declare var $:any;
+import { Router } from '@angular/router';
+
+declare var $: any;
 
 @Component({
   selector: 'app-registro-screen',
   templateUrl: './registro-screen.component.html',
-  styleUrls: ['./registro-screen.component.scss']
+  styleUrls: ['./registro-screen.component.scss'],
 })
-export class RegistroScreenComponent implements OnInit{
-
+export class RegistroScreenComponent implements OnInit {
   //Variables
-  public user:any = {};
-  public errors:any = {};
+  public user: any = {};
+  public errors: any = {};
 
   //Para contraseña
   public hide_1: boolean = false;
   public inputType_1: string = 'password';
 
   //Para las edades
-  public selectedValue: string = "";
+  public selectedValue: string = '';
   public edades: any[] = [];
 
   constructor(
-    private usuariosService: UsuariosService
-  ){}
+    private usuariosService: UsuariosService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
     this.user = this.usuariosService.esquemaUser();
     this.llenarArrayEdades();
   }
 
-  public llenarArrayEdades(){
+  public llenarArrayEdades() {
     for (let i = 18; i <= 80; i++) {
-      this.edades.push({value: i});
+      this.edades.push({ value: i });
     }
   }
 
-  public terminosCondiciones(){
+  public terminosCondiciones() {}
 
-  }
-
-  public registrar(){
+  public registrar() {
     //Validar
     this.errors = [];
 
     this.errors = this.usuariosService.validarUsuario(this.user);
-    if(!$.isEmptyObject(this.errors)){
+    if (!$.isEmptyObject(this.errors)) {
       return false;
     }
-
   }
 
-  public goLogin(){
-
+  public goLogin() {
+    this.router.navigate(['']);
   }
 
-
-  showPassword()
-  {
-    if(this.inputType_1 == 'password'){
+  showPassword() {
+    if (this.inputType_1 == 'password') {
       this.inputType_1 = 'text';
       this.hide_1 = true;
-    }
-    else{
+    } else {
       this.inputType_1 = 'password';
       this.hide_1 = false;
     }
   }
 
-  public isMobile(){
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(navigator.userAgent)){
+  public isMobile() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(
+        navigator.userAgent
+      )
+    ) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-
 }
